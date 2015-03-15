bd18175c22f55f075064b3f95963aa7e90c5e02b
pratik12 <p_bidkar@encs.concordia.ca>
2015-03-14 23:25:41 -0400

    Initial function to read from file..

--- ScriptRules/rules_index.rb
+++ ScriptRules/rules_index.rb
@@ -1,2 +1,51 @@
+#!/usr/local/bin/ruby
+require 'rubygems'
+require 'csv'
+require 'json'
+
 class RulesIndex
+
+
+ def initialize()
+
+ end
+
+  def readFromFile(fileName)
+    jsonFile = File.open('some.json','w');
+    jsonData = {}
+
+    CSV.foreach(fileName,:headers => true, :header_converters => :symbol) do |row|
+
+      jsonData['Emails'] = {
+          :subject => row[0],
+          :body => row[1],
+          :fromName => row[2],
+          :fromEmail => row[3],
+          :toName => row[5],
+          :toEmail => row[6]
+
+      };
+      # puts "data is #{jsonData[jsonData.length-1]}"
+
+      jsonFile.write(JSON.pretty_generate(jsonData));
+
+     # CSV.parse(row).to_json
+     # @jsonFile.write(JSON.pretty_generate(@jsonData));
+   #   csv = CSV.new(fileName, :headers => true, :header_converters => :symbol, :converters => :all)
+    #  csv.to_a.map {|row| row.to_hash }
+    end
+
+
+    #{data = File.open(fileName,"r")
+    #until data.eof
+    #  line = data.readline()
+    #  print line
+    #end}"
+  end
+
+
+
 end
+
+rulesIndexObj = RulesIndex. new
+rulesIndexObj.readFromFile("C://Users//Pratik Bidkar//RubymineProjects//Email-Recognadion-Script//csvInput.csv")
